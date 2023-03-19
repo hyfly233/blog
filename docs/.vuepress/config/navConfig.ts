78a34d23f66c88fd99cda070bd43daae75c2e3e5
hyfly233 <hyfly233@outlook.com>
2023-03-19 21:00:26 +0800

    Update navConfig.ts

--- docs/.vuepress/config/navConfig.ts
+++ docs/.vuepress/config/navConfig.ts
@@ -1,66 +1,65 @@
 export const navConfig = [
     { text: 'Home', link: '/' },
     {
-        text: 'Back End',
+        text: '基础',
         children: [
             {
                 text: 'Java',
                 children: [
-                    { text: 'Basis', link: '/md/backEnd/java/basis/content' },
-                    { text: 'MultiThread', link: '/md/backEnd/java/multiThread/multiThread' },
-                    { text: 'JVM', link: '/md/backEnd/java/jvm/jvm' },
-                    { text: 'IO', link: '/md/backEnd/java/io/io' },
-                    { text: 'Spring', link: '/md/backEnd/java/spring/spring' },
-                    { text: 'Spring MVC', link: '/md/backEnd/java/springMvc/springMvc' },
-                    { text: 'Spring Boot', link: '/md/backEnd/java/springBoot/springBoot' },
-                    { text: 'Spring Cloud', link: '/md/backEnd/java/springCloud/content' },
+                    { text: 'Java基础', link: '/md/基础/Java基础.md' },
+                    { text: '面向对象', link: '/md/基础/面向对象.md' },
+                    { text: 'Java进阶', link: '/md/基础/Java进阶.md' },
+                    { text: 'Java反射', link: '/md/基础/反射.md' },
+                    { text: 'Java调优', link: '/md/基础/Java调优.md' },
+                    { text: '集合', link: '/md/基础/集合.md' },
+                    { text: '多线程', link: '/md/基础/多线程.md' },
+                    { text: '线程池', link: '/md/基础/线程池.md' },
+                    { text: '锁', link: '/md/基础/锁.md' },
                 ]
             }
         ]
     },
     {
-        text: 'Database',
+        text: '数据库',
         children: [
-            { text: 'Basis', link: '/md/database/basis/basis' },
-            { text: 'MongoDB', link: '/md/database/mongoDB/mongoDB' },
-            { text: 'MySQL', link: '/md/database/mysql/mysql' },
+            { text: '数据库索引', link: '/md/数据库/SQL.md' },
+            { text: '数据库锁与事务', link: '/md/数据库/数据库索引.md' },
+            { text: '数据库优化', link: '/md/数据库/数据库优化.md' },
         ]
     },
     {
-        text: 'Middleware',
+        text: 'Spring',
+        children: [
+            { text: 'Spring', link: '/md/Spring/Spring.md' },
+            { text: 'Spring MVC', link: '/md/Spring/SpringMvc.md' },
+            { text: 'Spring Boot', link: '/md/Spring/SpringBoot.md' },
+        ]
+    },
+    {
+        text: '微服务',
         children: [
             {
-                text: 'Cache', children: [
-                    { text: 'Basis', link: '/md/middleware/cache/basis/basis' },
-                    { text: 'Redis', link: '/md/middleware/cache/redis/redis' },
+                text: 'Spring Cloud', children: [
+                    { text: 'Spring Cloud', link: '/md/微服务/SpringCloud.md' },
+                    { text: 'Spring Cloud Alibaba', link: '/md/微服务/SpringCloudAlibaba.md' },
+                    { text: 'Spring Cloud Netflix', link: '/md/微服务/SpringCloudNetflix.md' },
                 ]
             },
             {
-                text: 'Message', children: [
-                    { text: 'Basis', link: '/md/middleware/message/basis/basis' },
-                    { text: 'RabbitMQ', link: '/md/middleware/message/rabbitMQ/rabbitMQ' },
-                    { text: 'Kafka', link: '/' },
+                text: 'Zookeeper', children: [
+                    { text: 'Zookeeper', link: '/md/微服务/Zookeeper.md' },
                 ]
             },
             {
-                text: 'Search', children: [
-                    { text: 'Elastic Search', link: '/md/middleware/search/es/es' }
+                text: '分布式锁', children: [
+                    { text: '分布式锁', link: '/md/微服务/分布式锁.md' },
                 ]
             },
             {
-                text: 'Storage', children: [
-                    { text: 'Basis', link: '/md/middleware/storage/basis/basis' },
-                    { text: 'FastDFS', link: '/md/middleware/storage/fastDfs/fastDfs' }
+                text: '分布式事务', children: [
+                    { text: '分布式事务', link: '/md/微服务/分布式事务.md' },
                 ]
             }
         ]
-    },
-    {
-        text: 'Other',
-        children: [
-            { text: 'Algorithm', link: '/md/other/algorithm/algorithm' },
-            { text: 'Design Patterns', link: '/md/other/designPatterns/designPatterns' },
-            { text: 'Data Structure', link: '/md/other/dataStructure/dataStructure' },
-        ]
-    },
+    }
 ]
